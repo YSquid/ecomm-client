73d1757273f970df0d80f3e5bccc7851713043cb
Ysquid <ahk648@gmail.com>
2023-02-06 14:02:15 -0600

    basic login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./Login.css";
 
 function Login() {
+
   return (
-    <div>Login</div>
-  )
+    <section className="Login">
+      <div className="Login__form">
+        <form action="http://localhost:3000/login" method="POST">
+          <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
