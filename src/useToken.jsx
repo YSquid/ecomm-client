5dfd64b4488d48998be8435ffc233bce7a5c8c32
Ysquid <ahk648@gmail.com>
2023-02-09 12:25:28 -0600

    cleanup and commenting

--- src/useToken.jsx
+++ src/useToken.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 
 export default function useToken() {
+  //get token from session storage (taken from resposne from server on /login route)
   const getToken = () => {
     const tokenString = sessionStorage.getItem('token');
-    console.log(`Token string == ${tokenString}`)
     const userToken = JSON.parse(tokenString);
-    console.log(`userToken == ${userToken}`)
+    //first time we load, no user token, so use the optional chaining operator
     return userToken?.token
   };
 
+  //set token state by calling getToken()
   const [token, setToken] = useState(getToken());
 
+  //save the token in sessionStorage, and setToken state
   const saveToken = userToken => {
     sessionStorage.setItem('token', JSON.stringify(userToken));
     setToken(userToken.token);
   };
 
+  //return an object with our token, and our setToken function
+  //setToken function is passed down to other elements from App for when they need to render login
   return {
     setToken: saveToken,
     token
