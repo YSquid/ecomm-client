c81d44eb9fa4494adbb00dffebe59f2514bb5a9f
Ysquid <ahk648@gmail.com>
2023-02-14 11:41:08 -0600

    basics of order page done

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
+import OrderDetail from "../orderDetail/OrderDetail";
 import "./Orders.css";
 
 function Orders({ token }) {
   const [orders, setOrders] = useState();
+  const [showDetails, setShowDetails] = useState([]);
+
+  const handleDetailsClick = (orderId) => {
+    if (showDetails.includes(orderId)) {
+      setShowDetails(showDetails.filter((order) => {
+        return order !== orderId
+      }))
+    } else {
+      setShowDetails([...showDetails, orderId]);
+    }
+  };
+
   const getOrders = async () => {
     const response = await fetch(
       `http://localhost:3000/api/orders/user/${token}`,
@@ -29,9 +42,17 @@ function Orders({ token }) {
               const date = new Date(order.add_time).toDateString();
               return (
                 <div className="orderItem" key={order.id}>
-                  <p>Order ID: {order.id}</p>
-                  <p>Order Date: {date}</p>
-                  <button type="submit" className="details">Details</button>
+                  <div className="orderBasic">
+                    <p>Order ID: {order.id}</p>
+                    <p>Order Date: {date}</p>
+                    <button
+                      className="details"
+                      onClick={() => handleDetailsClick(order.id)}
+                    >
+                      Details
+                    </button>
+                    {showDetails.includes(order.id) ? <OrderDetail order_id={order.id} /> : null}
+                  </div>
                 </div>
               );
             })
