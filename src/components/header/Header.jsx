a3f466349ab6ca876f7f949f5a012a88bec451ec
Ysquid <ahk648@gmail.com>
2023-02-16 11:32:24 -0600

    more header styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { FaHamburger } from "react-icons/fa";
-import { AiOutlineShoppingCart, AiOutlineShopping} from 'react-icons/ai'
+import { AiOutlineHome, AiOutlineShoppingCart, AiOutlineShopping} from 'react-icons/ai'
 import Logout from '../logout/Logout'
 
 function Header({ token, setToken }) {
@@ -10,13 +10,13 @@ function Header({ token, setToken }) {
     <header className="header">
       <div className="title">
         <h1>
-          Ahmad's Eats <FaHamburger />
+          <Link to="/">Ahmad's Eats<FaHamburger className="hamburger" /></Link>
         </h1>
       </div>
       <nav className="nav">
         {token && (
           <div className="navLoggedIn">
-            <Link to="/" title="shop">Return to Shop</Link>
+            <Link to="/" title="shop"><AiOutlineHome/></Link>
             <Link to="/cart" title="Cart"><AiOutlineShoppingCart/></Link>
             <Link to="/orders" title="Orders"><AiOutlineShopping/></Link>
             <Logout setToken={setToken}/>
