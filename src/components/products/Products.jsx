a018417bb6863044da4bfb360adb4616e0b49c36
Ysquid <ahk648@gmail.com>
2023-02-16 16:06:46 -0600

    link hover styles

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./Products.css";
 import Categories from "../categories/Categories";
 import ProductCard from "../productCard/ProductCard";
+import { CircularProgress } from "@mui/material";
 
 function Products({token}) {
   //categories stored here, but set by the Categories component
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   //get all products on products and store in state on render
@@ -17,11 +19,12 @@ function Products({token}) {
     const responseJSON = await response.json();
     setProducts(responseJSON);
     setFilteredProducts(responseJSON)
+    setIsLoading(false);
   };
 
   //runs on render to get all products
   useEffect(() => {
-    getProducts()
+    getProducts();
   }, [])
 
   //filters products each time categories updates
@@ -32,6 +35,12 @@ function Products({token}) {
 
     setFilteredProducts(filteredProducts)
   }, [categories, products]);
+
+  if (isLoading) {
+    return (
+      <CircularProgress />
+    )
+  }
 
   return (
     <section className="products">
