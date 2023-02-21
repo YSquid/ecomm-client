21af4f69e2421ef3000275e3ae47d797a0372ea3
Ysquid <ahk648@gmail.com>
2023-02-21 15:37:48 -0600

    lazy logout

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -6,15 +6,15 @@ function Logout({ setToken }) {
   const navigate = useNavigate();
   const baseURL = process.env.NODE_ENV === 'production' ? 'https://ahmads-eats-api.netlify.app' : 'http://localhost:3000';
   const logout = async () => {
-    const response = await fetch(`${baseURL}/logout`, {
-      credentials: 'include',
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
+    // const response = await fetch(`${baseURL}/logout`, {
+    //   credentials: 'include',
+    //   method: "POST",
+    //   headers: {
+    //     "Content-type": "application/json",
+    //   },
+    // });
 
-        console.log(response)
+        // console.log(response)
         setToken(null);
         sessionStorage.removeItem('token')
         return navigate('/');
