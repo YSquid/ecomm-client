d299b538084d7d91d2204e430c39e10148b8e27a
Ysquid <ahk648@gmail.com>
2023-02-22 15:08:15 -0600

    updated logout

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -3,20 +3,18 @@ import "./Logout.css";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 
-function Logout({ setToken }) {
+function Logout({ setUser }) {
   const navigate = useNavigate();
-  // const baseURL = process.env.NODE_ENV === 'production' ? 'https://ahmads-eats-api.netlify.app' : 'http://localhost:3000';
+  const baseURL = process.env.NODE_ENV === 'production' ? 'https://ahmads-eats-api.netlify.app' : 'http://localhost:3000';
   const logout = async () => {
-    // const response = await fetch(`${baseURL}/logout`, {
-    //   credentials: 'include',
-    //   method: "POST",
-    //   headers: {
-    //     "Content-type": "application/json",
-    //   },
-    // });
-    sessionStorage.removeItem("token")
-    setToken();
-    return navigate("/");
+    const response = await fetch(`${baseURL}/api/auth/logout`, {
+      credentials: 'include',
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    setUser(false);
   };
   return (
     // <form onSubmit={logout} className='logout'>
